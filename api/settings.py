12ccec0b787b96a20171c8f8db73eb86bc3a8d7d
MokkoFm <mokkofmpoetry@gmail.com>
2020-12-14 16:36:15 +0200

    build config, add admin

--- api/settings.py
+++ api/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from environs import Env
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n9b%r%uk%zr4%j3!pz*2g$s=)+gcwfl%$n9^nqfd%=!imq09ft'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,10 +81,14 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+    } 
+}  
+
 
 
 # Password validation
@@ -106,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Helsinki'
 
 USE_I18N = True
 
@@ -119,3 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,96 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(verbose_name='title', max_length=50)
+    description = models.TextField(verbose_name='description', blank=True)
+    price = models.DecimalField(verbose_name='price', max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to="images", verbose_name="image of product")
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def image_url(self):
+        try:
+            url = self.image.url
+        except AttributeError:
+            url = ''
+        return url
+
+    class Meta:
+        verbose_name = "product"
+        verbose_name_plural = "products"
+
+
+class Customer(models.Model):
+    firstname = models.CharField(max_length=50, verbose_name="firstname")
+    lastname = models.CharField(max_length=50, verbose_name="lastname")
+    phonenumber = models.CharField(
+        max_length=20, blank=True, verbose_name="phonenumber")
+    email = models.CharField(
+        max_length=100, blank=True, verbose_name="e-mail")
+    address = models.TextField(verbose_name="address", null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+    class Meta:
+        verbose_name = "customer"
+        verbose_name_plural = "customers"
+
+
+class Order(models.Model):
+    PAYMENT_METHOD = [
+        ('Cash', 'By cash'),
+        ('Card', 'By card'),
+    ]
+
+    registrated_at = models.DateTimeField(
+        default=timezone.now, verbose_name='data of registration')
+    payment_method = models.CharField(
+        max_length=4, choices=PAYMENT_METHOD,
+        default='Неизвестно', verbose_name='payment method')
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE,
+        related_name='orders', verbose_name='customer of order', null=True)
+
+    def __str__(self):
+        return f" Order number - {self.id}"
+
+    @property
+    def cart_total(self):
+        return sum([item.total for item in self.order_items.all()])
+
+    @property
+    def cart_items_amount(self):
+        return sum([item.quantity for item in self.order_items.all()])
+
+    class Meta:
+        verbose_name = 'order'
+        verbose_name_plural = 'orders'
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE,
+        related_name='product_items')
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE,
+        related_name='order_items')
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(25)],
+        verbose_name='quantity', default=0)
+
+    def __str__(self):
+        return f"{self.product}: {self.quantity}"
+
+    @property
+    def total(self):
+        return self.product.price * self.quantity
+
+    class Meta:
+        verbose_name = "order item"
+        verbose_name_plural = "order items"
