1487891552f25c3765990ee06c0cb9bd2f7722c0
MokkoFm <mokkofmpoetry@gmail.com>
2020-12-15 17:14:45 +0200

    add views

--- cart/views.py
+++ cart/views.py
@@ -1,13 +1,50 @@
 from django.shortcuts import render
+from cart.models import Product, Customer, Order, OrderItem
+from cart.serializers import OrderSerializer, CustomerSerializer
+from cart.serializers import OrderItemSerializer, ProductSerializer
+from rest_framework import generics
+from rest_framework.serializers import ValidationError
 
-# Create your views here.
+
 def index(request):
-    return render(request, "index.html")
+    products = Product.objects.all()
+    context = {
+        "products": products,
+    }
+    return render(request, "index.html", context)
 
 
-def register_order(request):
-    return render(request)
+class OrderCreateView(generics.CreateAPIView):
+    serializer_class = OrderSerializer
 
 
-def product_list_api(request):
-    return render(request)
+class OrderListView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+
+class CustomerCreateView(generics.CreateAPIView):
+    serializer_class = CustomerSerializer
+
+
+class CustomersListView(generics.ListAPIView):
+    serializer_class = CustomerSerializer
+    queryset = Customer.objects.all()
+
+
+class OrderItemCreateView(generics.CreateAPIView):
+    serializer_class = OrderItemSerializer
+
+
+class ProductCreateView(generics.CreateAPIView):
+    serializer_class = ProductSerializer
+
+
+class ProductsListView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
