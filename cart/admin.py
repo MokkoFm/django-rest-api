cb87baf22db3bd0af7b9c7bda941e66c08575bb4
MokkoFm <mokkofmpoetry@gmail.com>
2020-12-15 17:14:24 +0200

    add order items as inline of order in admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from cart.models import Product, Customer, Order, OrderItem
+from cart.models import Product, Customer, Order, OrderItem, Country
 # Register your models here.
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
 
 admin.site.register(Product)
 admin.site.register(Customer)
-admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Country)
