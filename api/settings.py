38fa8933485f28c31ee923298553337034674ea4
MokkoFm <mokkofmpoetry@gmail.com>
2020-12-16 01:05:19 +0200

    add allowed hosts

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cart-rest-api.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
